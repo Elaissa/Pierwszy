d9fa51ba4eb70e50e45769d49ecb8af400e4e189
Developer <awszelaka9@gmail.com>
2016-12-27 17:56:58 +0100

    Change text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witaj w Gitubie!");
+        System.out.println("Hello Gitubie!");
 	// write your code here
     }
 }
